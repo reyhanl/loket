09a32c186b93252469000e8e62796d11df61347e
Rey <artube090@gmail.com>
2020-01-26 19:50:48 +0700

    api req success

---
+++ Loker/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Loker
+//
+//  Created by Reyhan on 24/01/20.
+//  Copyright © 2020 reyhan. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
